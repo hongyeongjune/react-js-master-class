bded87bc33be0b5943fac03857c7b45ba4bf16a6
hongyeongjune <hongyeongjune@naver.com>
2022-09-22 09:15:32 +0900

    Our First Styled Component

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,7 +1,30 @@
+import styled from 'styled-components';
+
 function App() {
+  const Father = styled.div`
+    display: flex;
+  `;
+  const BoxOne = styled.div`
+    background-color: teal;
+    width: 100px;
+    height: 100px;
+  `;
+  const BoxTwo = styled.div`
+    background-color: tomato;
+    width: 100px;
+    height: 100px;
+  `;
+  const Hello = styled.h1`
+    color: white;
+  `;
+
   return (
-    <div className="App">
-    </div>
+    <Father>
+      <BoxOne>
+        <Hello>Hello</Hello>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
   );
 }
 
