76490b130b1479d999456845aa89351a03e0d6fc
hongyeongjune <hongyeongjune@naver.com>
2022-09-27 09:31:30 +0900

    [CRYPTO TRACKER] Setup

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,93 +1,9 @@
-import styled, { keyframes } from 'styled-components';
-import Circle from './Circle';
-import React, { useState } from 'react';
+import React from 'react';
+import Router from './Router';
 
 function App() {
-  const Wrapper = styled.div`
-    display: flex;
-    height: 100vh;
-    width: 100vw;
-    justify-content: center;
-    align-items: center;
-    background-color: ${props => props.theme.backgroundColor}
-  `;
-  const Title = styled.h1`
-    color: ${props => props.theme.textColor};
-  `;
-  const Box = styled.div`
-    background-color: ${props => props.theme.backgroundColor};
-    width: 100px;
-    height: 100px;
-  `;
-  const Hello = styled.h1`
-    color: white;
-  `;
-  // const Circle = styled(Box)`
-  //   border-radius: 50px;
-  // `;
-  const Input = styled.input.attrs({ required: true })`
-    background-color: tomato;
-  `;
-  const animation = keyframes`
-    0% {
-        transform: rotate(0deg);
-        border-radius: 0px;
-    }
-    50% {
-        border-radius: 100px;
-    }
-    100% {
-        transform: rotate(360deg);
-        border-radius: 0px;
-    }
-  `;
-  const Emoji = styled.span`
-    font-size: 36px;
-  `;
-  const TomatoBox = styled.div`
-    height: 200px;
-    width: 200px;
-    background-color: tomato;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    animation: ${animation} 1s linear infinite;
-    ${Emoji}:hover {
-        font-size: 98px;
-    }
-  `;
-  const Container = styled.div`
-    background-color: ${props => props.theme.backgroundColor};
-  `
-  const H1 = styled.h1`
-    color: ${props => props.theme.textColor};
-  `
-
-  const [value, setValue] = useState<string>("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setValue(event.currentTarget.value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Hello, ", value);
-  }
-
   return (
-    <Container>
-      <Circle backgroundColor="teal" borderColor="yellow" />
-      <Circle backgroundColor="tomato" />
-      <form onSubmit={onSubmit}>
-        <input
-            value={value}
-            type="text"
-            placeholder="username"
-            onChange={onChange}
-        />
-      </form>
-      <H1>Hello</H1>
-    </Container>
+    <Router />
   );
 }
 
--- react-masterclass/src/index.tsx
+++ react-masterclass/src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
-import { darkTheme } from './theme';
+import { lightTheme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={lightTheme}>
         <App />
     </ThemeProvider>
 );
