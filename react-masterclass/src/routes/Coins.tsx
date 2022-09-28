093f027cfbab8d362feb5b16eed1ba28dfe97340
hongyeongjune <hongyeongjune@naver.com>
2022-09-28 09:23:31 +0900

    [CRYPTO TRACKER] Home part One

--- react-masterclass/src/routes/Coins.tsx
+++ react-masterclass/src/routes/Coins.tsx
@@ -1,12 +1,63 @@
+import { Link } from 'react-router-dom'
 import styled from 'styled-components';
 
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: ${props => props.theme.textColor};
+  color: ${props => props.theme.backgroundColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    display: block;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
 const Title = styled.h1`
-    color: ${props => props.theme.accentColor}
-`
+  font-size: 48px;
+  color: ${props => props.theme.accentColor};
+`;
 
 function Coins() {
+    const coins = [
+        {
+            id: "btc-button",
+            name: "name"
+        }
+    ];
     return (
-        <Title>Coins</Title>
+        <Container>
+            <Header>
+                <Title>Coins</Title>
+            </Header>
+            <CoinsList>
+                {
+                    coins.map((coin) => (
+                        <Coin key={coin.id}>
+                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                        </Coin>
+                    ))
+                }
+            </CoinsList>
+        </Container>
     );
 }
 
