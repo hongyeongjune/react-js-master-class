0887f63dde59ec426a98f7717c1b7f02e9fb84fc
hongyeongjune <hongyeongjune@naver.com>
2022-09-29 09:08:25 +0900

    [CRYPTO TRACKER] Router States

--- react-masterclass/src/routes/Coin.tsx
+++ react-masterclass/src/routes/Coin.tsx
@@ -1,9 +1,47 @@
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface RouterState {
+    name: string;
+}
 
 function Coin() {
-    const { coinId } = useParams<{coinId: string}>();
+    const location = useLocation();
+    const state = location.state as RouterState;
+    // const [loading, setLoading] = useState(true);
+
+    console.log(location);
+
     return (
-        <h1>Coin: {coinId}</h1>
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading..."}</Title>
+            </Header>
+        </Container>
     );
 }
 
